94d957f2a8cedf25666773ec50d23a03a5d8af1f
Asaf Gardin <asaf@interai.co>
2021-03-26 11:15:55 +0300

    Added song and changed onclick

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,38 @@ import './App.css';
 import {AnswerButton, ButtonColor} from "./components/button/answer-button";
 import ClearIcon from '@material-ui/icons/Clear';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import {Test} from "./components/test";
+
+import {Howl} from 'howler';
+// @ts-ignore
+import BackgroundSong from "./resources/audio/yesSong.mp3";
 
 const MARRY_YOU_URL = "https://www.youtube.com/watch?v=Zlv1rdcpS9M";
-// const BACKGROUND_MUSIC_URL = "./src/resources/audio/Assaf.png";
 
 
 function App() {
     const [buttonX, setButtonX] = useState(0);
     const [buttonY, setButtonY] = useState(0);
-    // const [play] = useSound(BACKGROUND_MUSIC_URL, {volume: 0.5});
+    const [marryYouID, setMarryYouID] = useState<number>();
+
+    const sound = new Howl({
+        src: [BackgroundSong]
+    });
+
+    // useEffect(() => {
+    //     // Playing song in the background on first render
+    //     sound.play();
+    // }, [])
 
 
-    const onYesRedirect = () => {
-        window.location.assign(MARRY_YOU_URL);
-        // window.open("", "_blank")
+    const onYesClick = (): void => {
+        if (marryYouID) {
+            sound.stop(marryYouID);
+            setMarryYouID(-1);
+        } else {
+            const songID = sound.play();
+            setMarryYouID(songID);
+        }
+
     }
 
     const onNoButtonHover = (event: MouseEvent) => {
@@ -33,25 +50,20 @@ function App() {
                 This is a Cool Project!
             </header>
 
-            <Test/>
             <div className={"button-wrapper"}>
 
                 <AnswerButton text={"No"}
                               color={ButtonColor.Red}
                               x={buttonX}
                               y={buttonY}
-                    // onClick={() => {
-                    //     console.log("playing");
-                    //     play();
-                    // }}
-                    // onHover={onNoButtonHover}
+                              onHover={onNoButtonHover}
                               icon={<ClearIcon/>}/>
 
 
                 <AnswerButton text={"Yes"}
                               color={ButtonColor.Green}
                               icon={<FavoriteIcon/>}
-                              onClick={onYesRedirect}/>
+                              onClick={onYesClick}/>
             </div>
         </div>
     );
