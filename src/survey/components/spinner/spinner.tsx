db08d386f3eec10e6d5a93db704cdab6f99cfc8d
Asaf Gardin <asaf@interai.co>
2021-10-16 15:36:12 +0300

    Changed spinner color

--- src/survey/components/spinner/spinner.tsx
+++ src/survey/components/spinner/spinner.tsx
@@ -1,23 +1,9 @@
 import React from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
-import {createStyles, makeStyles, Theme} from "@material-ui/core";
 import ClipLoader from "react-spinners/ClipLoader";
 import "./spinner.css";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            display: 'flex',
-            '& > * + *': {
-                marginLeft: theme.spacing(2),
-            },
-        },
-    }),
-);
-
 export const CircularSpinner = () => {
-    const style = useStyles();
-
     return (
         <div>
             <CircularProgress/>
@@ -35,7 +21,7 @@ export const HashLoader: React.FC<HashLoaderProps> = (
     }) => {
     return (
         <div className={"hash-spinner-wrapper"}>
-            <ClipLoader color={"white"} loading={isLoading} size={100} />
+            <ClipLoader color={"#c3a46c"} loading={isLoading} size={100}/>
         </div>
     )
 }
