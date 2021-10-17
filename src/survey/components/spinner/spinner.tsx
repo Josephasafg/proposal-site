b3d8e807557b15480005f3aebc10041bac0610d0
Asaf Gardin <asaf@interai.co>
2021-10-17 21:42:55 +0300

    Fixed color

--- src/survey/components/spinner/spinner.tsx
+++ src/survey/components/spinner/spinner.tsx
@@ -2,26 +2,41 @@ import React from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import ClipLoader from "react-spinners/ClipLoader";
 import "./spinner.css";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+const useSpinnerStyles = makeStyles((_: Theme) =>
+    createStyles({
+        colorPrimary: {
+            color: "#c3a46c",
+        },
+
+    }),
+);
+
+
 
 export const CircularSpinner = () => {
+    const style = useSpinnerStyles()
     return (
-        <div>
+        <div className={style.colorPrimary}>
             <CircularProgress/>
         </div>
     )
 }
 
 interface HashLoaderProps {
-    isLoading: boolean
+    isLoading: boolean,
+    size?: number
 }
 
 export const HashLoader: React.FC<HashLoaderProps> = (
     {
-        isLoading
+        isLoading,
+        size = 100
     }) => {
     return (
         <div className={"hash-spinner-wrapper"}>
-            <ClipLoader color={"#c3a46c"} loading={isLoading} size={100}/>
+            <ClipLoader color={"#c3a46c"} loading={isLoading} size={size}/>
         </div>
     )
 }
