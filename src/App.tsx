ed39b73ba249f746d92fd980887eb9e610b06b68
Asaf Gardin <asaf@interai.co>
2021-11-01 00:22:11 +0200

    Removed songs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
-import {ProposalPage} from "./pages/proposal/propsal-page";
 import SurveyPage from "./pages/survey/survey-page";
 import {WelcomePage} from "./pages/survey/welcome-page/welcome-page";
 import {WinningSong} from './survey/components/winning-song/winning-song';
