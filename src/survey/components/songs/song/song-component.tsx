871ef469f8d22609633071fbc7459fcae2ba5b9e
Asaf Gardin <asaf@interai.co>
2021-11-01 23:49:22 +0200

    Removed images

--- src/survey/components/songs/song/song-component.tsx
+++ src/survey/components/songs/song/song-component.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./song.css";
 import {Song} from "../../../models/song";
-import BELIEVER from "./images/believer.jpg";
-import HIGH_HOPES from "./images/High_Hopes.jpg";
-import HALLELUJAH from "./images/hallelujah.jpg";
-import BEAUTIFUL_DAYS from "./images/beautiful_days.jpg";
+// import BELIEVER from "./images/believer.jpg";
+// import HIGH_HOPES from "./images/High_Hopes.jpg";
+// import HALLELUJAH from "./images/hallelujah.jpg";
+// import BEAUTIFUL_DAYS from "./images/beautiful_days.jpg";
 // @ts-ignore
 // import ReactRoundedImage from "react-rounded-image";
 
 
-const SONG_IMAGES: { [key: number]: string } = {
-    1: BELIEVER,
-    2: HIGH_HOPES,
-    3: BEAUTIFUL_DAYS,
-    4: HALLELUJAH,
-}
+// const SONG_IMAGES: { [key: number]: string } = {
+//     1: BELIEVER,
+//     2: HIGH_HOPES,
+//     3: BEAUTIFUL_DAYS,
+//     4: HALLELUJAH,
+// }
 
 interface SongProps {
     song: Song
