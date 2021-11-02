0e0478fea14b7c3a471d5cbce124a07706fd68f3
Asaf Gardin <asaf@interai.co>
2021-11-03 00:27:25 +0200

    Added history

--- src/survey/components/main-page/main-page.tsx
+++ src/survey/components/main-page/main-page.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import SongList from "../songs/songs";
 import {CircularSpinner} from "../spinner/spinner";
 import {SubmitButton} from "../submit-button/submit-button";
@@ -6,7 +6,7 @@ import {Song} from "../../models/song";
 import {VoteCountdown} from "../countdown-clock/countdown-clock";
 import {SongSubmissionContext} from "../../../pages/survey/survey-page";
 import "./main-page.css";
-import {ThankYouPage} from "../thank-you-page/thank-you-page";
+import {useHistory} from "react-router-dom";
 
 const HELP_US_TEXT = "שוברים את הכוס!";
 const HELP_US_DESCRITPION = 'בחרו שיר מתוך הרשימה';
@@ -26,6 +26,7 @@ export const MainPage: React.FC<MainPageProps> = (
     const {id} = useContext(SongSubmissionContext);
     const [isLoad, setIsLoad] = useState(false);
     const [hasSubmitted, setHasSubmitted] = useState(false);
+    const history = useHistory();
 
     const handleOnSubmit = async () => {
         setIsLoad(true);
@@ -34,23 +35,26 @@ export const MainPage: React.FC<MainPageProps> = (
         setHasSubmitted(hasSuccessfulSubmission)
     }
 
+    useEffect(() => {
+        if (hasSubmitted)
+            history.push("/survey/thank-you");
+    }, [hasSubmitted])
+
     return (
         <div className={"site-background"}>
-            {hasSubmitted ? <ThankYouPage/> :
-                <div>
-                    <div className="help-us-header">
-                        <div className={"help-us-text"}>{HELP_US_TEXT}</div>
-                        <div className={"help-us-description"}>{HELP_US_DESCRITPION}</div>
-                        <div className={"help-us-description"}>{HELP_US_DESCRITPION2}</div>
-                    </div>
-
-                    <SongList songs={songs}/>
-                    <div className={"vote-countdown-wrapper"}>
-                        <VoteCountdown/>
-                    </div>
-                    {isLoad ? <CircularSpinner/> : <SubmitButton onClick={handleOnSubmit} pickedSongId={id}/>}
+            <div>
+                <div className="help-us-header">
+                    <div className={"help-us-text"}>{HELP_US_TEXT}</div>
+                    <div className={"help-us-description"}>{HELP_US_DESCRITPION}</div>
+                    <div className={"help-us-description"}>{HELP_US_DESCRITPION2}</div>
                 </div>
-            }
+
+                <SongList songs={songs}/>
+                <div className={"vote-countdown-wrapper"}>
+                    <VoteCountdown/>
+                </div>
+                {isLoad ? <CircularSpinner/> : <SubmitButton onClick={handleOnSubmit} pickedSongId={id}/>}
+            </div>
 
         </div>
     )
