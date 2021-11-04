ad75c149635c39feba26ea05d93b262e22ce9f23
Asaf Gardin <asaf@interai.co>
2021-11-05 01:08:21 +0200

    Fixed alignment of loader

--- src/survey/components/winning-song/winning-song.tsx
+++ src/survey/components/winning-song/winning-song.tsx
@@ -18,21 +18,19 @@ export const WinningSong: React.FC = () => {
     }, [])
 
     return (
-        <div className={"App"}>
-            <div className={"winning-song-page-wrapper"}>
-                {winningSong ?
-                    <div className={"winning-song-wrapper"}>
-                        <div className={"winning-song-description"}>
-                            {WINNING_SONG_TEXT}
-                        </div>
+        <div className={"winning-song-page-wrapper"}>
+            {winningSong ?
+                <div className={"winning-song-wrapper"}>
+                    <div className={"winning-song-description"}>
+                        {WINNING_SONG_TEXT}
+                    </div>
 
-                        <div>
-                            {winningSong.name}
-                        </div>
-                    </div> :
-                    <CircularSpinner/>
-                }
-            </div>
+                    <div>
+                        {winningSong.name}
+                    </div>
+                </div> :
+                <CircularSpinner/>
+            }
         </div>
     )
 }
