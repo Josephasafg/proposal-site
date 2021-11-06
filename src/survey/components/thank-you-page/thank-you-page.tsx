db88dc9c3c44ae5a6f7de07fb3f6f68dc191ca92
Asaf Gardin <asaf@interai.co>
2021-11-07 00:58:20 +0200

    Added more text

--- src/survey/components/thank-you-page/thank-you-page.tsx
+++ src/survey/components/thank-you-page/thank-you-page.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./thank-you.css";
 
 const THANK_YOU_TEXT = "תודה על הצבעתכם, עכשיו לכו תהנו!";
-const US_TEXT = "אוהבים, אסף ושרון"
+const US_TEXT = "אוהבים,"
+const US_TEXT2 = "אסף ושרון"
 
 export const ThankYouPage: React.FC = () => {
     return (
@@ -12,8 +13,12 @@ export const ThankYouPage: React.FC = () => {
                     {THANK_YOU_TEXT}
                 </div>
 
+                <br/>
                 <div>
                     {US_TEXT}
+                </div>
+                <div>
+                    {US_TEXT2}
                 </div>
             </div>
         </div>
