5109b0d32e0dbc4203ce205a1868fabe38a2731b
Asaf Gardin <asaf@interai.co>
2022-02-19 12:23:01 +0200

    Updated date

--- src/survey/ending-vote-time.ts
+++ src/survey/ending-vote-time.ts
@@ -1,4 +1,4 @@
-export const CHOSEN_SONG_DATE = new Date(2021, 10, 8, 20, 30);
+export const CHOSEN_SONG_DATE = new Date(2022, 10, 8, 20, 30);
 
 export const isVotingEnded = (): boolean => {
     return new Date() > CHOSEN_SONG_DATE;
